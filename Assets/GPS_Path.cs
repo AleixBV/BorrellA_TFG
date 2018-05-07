6b9d0be35d40497dbb19ffab51c02c0fd685b68e
Aleix <aleixborrellvives@gmail.com>
2018-05-07 02:37:22 +0200

    Make and show the path in the GPS

---
+++ Assets/GPS_Path.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class GPS_Path : MonoBehaviour {
+
+    [SerializeField] private Transform destination;
+    [SerializeField] private LineRenderer line;
+    [SerializeField] private NavMeshAgent agent;
+    [SerializeField] private float timer_recalculate = 1.0f;
+
+    private float timer_recalculate_progress = 0.0f;
+
+    // Use this for initialization
+    void Start () {
+        SetPath();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        timer_recalculate_progress += Time.deltaTime;
+
+        if(timer_recalculate_progress >= timer_recalculate)
+        {
+            timer_recalculate_progress = 0.0f;
+
+            SetPath();
+        }
+	}
+
+    void SetPath()
+    {
+        agent.isStopped = true;
+        agent.SetDestination(destination.position);
+        //Debug.Log("recalcule path gps");
+
+        StopAllCoroutines();
+        StartCoroutine(DrawPath());
+    }
+
+    IEnumerator DrawPath()
+    {
+        yield return new WaitUntil(() => !agent.pathPending);
+
+        if (agent.path.corners.Length < 2)
+        {
+            Debug.Log("gps path length too small");
+            yield break;
+        }
+
+        line.positionCount = agent.path.corners.Length;
+
+        for (int x = 0; x < agent.path.corners.Length; x++)
+            line.SetPosition(x, agent.path.corners[x]);
+    }
+}
